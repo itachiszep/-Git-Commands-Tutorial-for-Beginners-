786117cbcc08a62c750300dbdef02ae113d42792
itachiszep <itachiszep@int.pl>
2025-10-03 21:59:37 +0200

    Update and rename README.md to github.jsx

--- README.md
+++
@@ -1,1 +0,0 @@
-# -Git-Commands-Tutorial-for-Beginners-
---
+++ github.jsx
@@ -0,0 +1,30 @@
+# -Git-Commands-Tutorial-for-Beginners-
+ Git Commands Tutorial for Beginners 
+
+git commands tutorial for beginners
+
+git --version
+
+ git branch 
+ 
+ git branch footer
+ 
+ git checkout footer
+ 
+ git checkout -b 
+ 
+ git push --set-upstream origin footer
+
+
+git init
+git add .
+git commit -m 'message of the commit
+git push
+
+git branch
+git branch branchName
+git checkout branchName
+
+git checkout -b branchName
+
+git pull
